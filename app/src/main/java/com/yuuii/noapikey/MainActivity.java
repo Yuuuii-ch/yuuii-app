570dadf1aa9e97847a28c85169136ddbaaecb2bf
Yuuuii-ch <ekahafid00@gmail.com>
2026-09-08 20:52:33 +0700

    Add file chooser functionality to MainActivity
    Implement file chooser support in WebView for file uploads.

--- app/src/main/java/com/yuuii/noapikey/MainActivity.java
+++ app/src/main/java/com/yuuii/noapikey/MainActivity.java
@@ -2,8 +2,12 @@ package com.yuuii.noapikey;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Message;
+import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -11,7 +15,10 @@ import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
 
+    private static final int FILE_CHOOSER_REQUEST_CODE = 51426;
+
     private WebView webView;
+    private ValueCallback<Uri[]> filePathCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +47,23 @@ public class MainActivity extends Activity {
 
     private class MainChromeClient extends WebChromeClient {
         @Override
+        public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> callback, FileChooserParams fileChooserParams) {
+            if (filePathCallback != null) {
+                filePathCallback.onReceiveValue(null);
+            }
+            filePathCallback = callback;
+
+            Intent intent = fileChooserParams.createIntent();
+            try {
+                startActivityForResult(intent, FILE_CHOOSER_REQUEST_CODE);
+            } catch (ActivityNotFoundException e) {
+                filePathCallback = null;
+                return false;
+            }
+            return true;
+        }
+
+        @Override
         public boolean onCreateWindow(WebView view, boolean isDialog, boolean isUserGesture, Message resultMsg) {
             final WebView popupWebView = new WebView(MainActivity.this);
             configureWebView(popupWebView);
@@ -63,6 +87,33 @@ public class MainActivity extends Activity {
             popupDialog.show();
             return true;
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode != FILE_CHOOSER_REQUEST_CODE) {
+            super.onActivityResult(requestCode, resultCode, data);
+            return;
+        }
+        if (filePathCallback == null) {
+            return;
+        }
+
+        Uri[] results = null;
+        if (resultCode == RESULT_OK && data != null) {
+            if (data.getClipData() != null) {
+                int count = data.getClipData().getItemCount();
+                results = new Uri[count];
+                for (int i = 0; i < count; i++) {
+                    results[i] = data.getClipData().getItemAt(i).getUri();
+                }
+            } else if (data.getData() != null) {
+                results = new Uri[]{data.getData()};
+            }
+        }
+
+        filePathCallback.onReceiveValue(results);
+        filePathCallback = null;
     }
 
     @Override
